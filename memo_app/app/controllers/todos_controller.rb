f97e2ab9aa18818510e98d25a969ff9150dfe497
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-01-09 21:08:10 +0900

    add_save_manegiment_commit

--- memo_app/app/controllers/todos_controller.rb
+++ memo_app/app/controllers/todos_controller.rb
@@ -1,2 +1,25 @@
 class TodosController < ApplicationController
+  def index
+    @todo = Todo.new
+    @todos = Todo.order('created_at ASC')
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      # redirect_to :root
+      respond_to do |format|
+      format.html { redirect_to :root }
+      format.json { render json: @todo}
+      end
+    else
+      # render :index, alert: 'Todoを入力してください'
+      render :index
+    end
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content)
+  end
 end
